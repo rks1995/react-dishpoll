65b59ffc41e8054db4063197398292db7fbea64c
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-08-03 22:03:31 +0530

    created dashboard

---
+++ frontend/src/assets/Wrappers/HomeWrapper.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const HomeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: 5em auto;
+  padding: 2em;
+`
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,43 @@
 import { Link } from 'react-router-dom'
+import { HomeWrapper } from '../assets/Wrappers/HomeWrapper'
+import styles from '../assets/styles/home.module.css'
+import Typewriter from 'typewriter-effect'
 
 function App() {
   return (
     <div className='App'>
-      <Link to={'/list'}>Start Voting</Link>
-      <Link to={'/list/results'}>Show Result</Link>
+      <HomeWrapper>
+        <div className={styles.links}>
+          <Link
+            className={[styles.button, styles.showDishes].join(' ')}
+            to={'/list'}
+          >
+            Start Voting
+          </Link>
+          <Link
+            className={[styles.button, styles.results].join(' ')}
+            to={'/list/results'}
+          >
+            Show Result
+          </Link>
+        </div>
+        {/* welcome text on dashboard */}
+        <Typewriter
+          options={{
+            strings: [
+              'Welcome To Dish Voting App!',
+              'Start Voting Your Favourite dishes!',
+            ],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 1,
+            delay: 40,
+            wrapperClassName: styles.welcomeText,
+            cursorClassName: styles.cursor,
+            pauseFor: 2500,
+          }}
+        />
+      </HomeWrapper>
     </div>
   )
 }
